a72349b809fb265434cb54eb241bea8a3141b9aa
Maksim Gridin <msgridin@betinvest.com>
2023-10-05 11:42:56 +0300

    .

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,3 @@
-pub const ELASTIC_URL: &str = "https://elastic.betinvest.work";
-pub const ELASTIC_LOGIN: &str = "1c_infra";
-pub const ELASTIC_PASSWORD: &str = "gdj1eaf3UYN2kct!vgk";
+pub const ELASTIC_URL: &str = "";
+pub const ELASTIC_LOGIN: &str = "";
+pub const ELASTIC_PASSWORD: &str = "";
